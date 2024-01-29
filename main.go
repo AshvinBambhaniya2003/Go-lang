bf45b9bbccf035061b1bc078a92c3c2ef50d33f7
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-01-29 12:08:55 +0530

    varible declaration

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+// jwttoken := 300000  connot declare this type
+var jwttoken = 4500
+
+const LoginToken string = "asjvon" 
+
 func main()  {
-	fmt.Println("hello world")
+	var username string = "ashvin"
+	fmt.Println(username)
+	fmt.Printf("variable of type %T \n",username)
+
+	var isLogin bool = false
+	fmt.Println(isLogin)
+	fmt.Printf("variable of type %T \n",isLogin)
+
+	// defult value of int
+	var anotherint int
+	fmt.Println(anotherint)
+	fmt.Printf("variable of type %T \n",anotherint)
+
+	numberoduser := 4000
+	fmt.Println(numberoduser)
+
+	fmt.Println(jwttoken)
+	fmt.Println(LoginToken)
+
 }
