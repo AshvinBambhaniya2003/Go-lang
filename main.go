1913bb6a79f358d59f5e58398a243ed035540c9c
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-01-30 15:34:39 +0530

    pointer

--- main.go
+++ main.go
@@ -1,32 +1,20 @@
 package main
 
 import "fmt"
-import "bufio"
-import "os"
-import "strconv"
-import "strings"
+// import "bufio"
+// import "os"
+// import "strconv"
+// import "strings"
 
-
-// jwttoken := 300000  connot declare this type
-var jwttoken = 4500
-
-const LoginToken string = "asjvon" 
 
 func main()  {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("enter the rating for pizza:")
+	var demoptr *int 
+	fmt.Println(demoptr)
 
-	// comma ok or err ok
+	mynumer  := 45
+	var ptr = &mynumer
+	fmt.Println(ptr,*ptr)
 
-	input , _ := reader.ReadString('\n')
-	fmt.Println("thank for ratindg")
-	fmt.Printf("type of input %T",input)
-
-	numRating,err  := strconv.ParseFloat(strings.TrimSpace(input),64)
-
-	if err != nil {
-		fmt.Println(err)
-	}else{
-		fmt.Println("added 1 to your rating",numRating+1)
-	}
+	*ptr = *ptr + 45
+	fmt.Println(mynumer)
 }
