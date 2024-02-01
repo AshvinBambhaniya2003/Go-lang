04e6e179ba610e284414d8e3afb532ea05c2258c
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-01 12:27:18 +0530

    defer

--- main.go
+++ main.go
@@ -3,28 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	ashvin := User{"ashvin", "ashvin@gmai.com", true, 20}
-	fmt.Println(ashvin)
-	// fmt.Printf("details of ashvin is %+v\n", ashvin)
-	// fmt.Printf("name is %v and email is %v", ashvin.Name, ashvin.Email)
 
-	ashvin.GetStatus()
-	ashvin.newMail()
-	fmt.Println(ashvin)  //original value is change only the copy of object is pass
+	defer fmt.Println("one")
+	defer fmt.Println("two")
+	defer fmt.Println("three")
+	fmt.Println("hello world")
+	myDefer()
 }
 
-type User struct {
-	Name   string
-	Email  string
-	Status bool
-	Age    int
-}
-
-func (u User) GetStatus(){
-	fmt.Println("is user active: ",u.Status)
-}
-
-func (u User) newMail() {
-	u.Email = "ashvinbambhaniya@gmail.com"
-	fmt.Println("email is :",u.Email)
+func myDefer(){
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
 }
