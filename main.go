1a24e5bdbd20520bf53bed54d0d52bf5da1f38c3
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-01 11:48:20 +0530

    function

--- main.go
+++ main.go
@@ -3,33 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	days := []string{"sun", "mon", "tue", "wen", "thur", "friday", "saturday"}
+	fmt.Println("hello from main")
+	greet()
 
-	for i := 0; i < len(days); i++ {
-		fmt.Println(days[i])
+	// func greest()  {
+	// 	fmt.Println("namste")
+	// }  -->this is not allow
+
+	result := sum(4,5)
+
+	fmt.Println(result)
+
+	// proresult := prosum(4,5,5,1,0,5)
+
+	proRes, proMessage := prosum(4,5,6,1,7)
+
+	fmt.Println(proRes,proMessage)
+}
+
+func sum(a int , b int) int {
+	return a + b
+}
+
+func prosum(values ...int) (int, string){
+	total := 0
+
+	for _, val := range values{
+		total = total + val
 	}
 
-	for _, day := range days {
-		fmt.Printf("value is %v\n", day)
-	}
+	return total,"hi from pro result function"
+}
 
-	roughval := 1
-
-	for roughval < 10 {
-
-		if roughval == 5 {
-			roughval++
-			// continue
-			// break
-			goto lco
-		}
-		fmt.Println(roughval)
-		roughval++
-	}
-	fmt.Println("outer of lco")
-lco:
-	fmt.Println("in goto statement")
-
-	fmt.Println("outer of lco")
-
+func greet()  {
+	fmt.Println("namste")
 }
