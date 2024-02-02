8c7f7d94b2650e8b3455e47fa21f04cf03e5049f
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-02 11:09:44 +0530

    hndeling panic

--- main.go
+++ main.go
@@ -1,33 +1,36 @@
 package main
+import "fmt"
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-)
+// recover function to handle panic
+func handlePanic() {
 
-const Url = "https://lco.dev"
+  // detect if panic occurs or not
+  a := recover()
 
-func main() {
-
-	response, err := http.Get(Url)
-
-	checkNilerr(err)
-
-	fmt.Printf("response type is %T",response)
-
-	defer response.Body.Close()
-
-	databytes, err := io.ReadAll(response.Body)
-
-	checkNilerr(err)
-
-	fmt.Println(string(databytes))
+  if a != nil {
+    fmt.Println("RECOVER", a)
+  }
 
 }
 
-func checkNilerr(err error) {
-	if err != nil {
-		panic(err)
-	}
+func division(num1, num2 int) {
+
+  // execute the handlePanic even after panic occurs
+  defer handlePanic()
+
+  // if num2 is 0, program is terminated due to panic
+  if num2 == 0 {
+    panic("Cannot divide a number by zero")
+  } else {
+    result := num1 / num2
+    fmt.Println("Result: ", result)
+  }
+}
+
+func main() {
+
+  division(4, 2)
+  division(8, 0)
+  division(2, 8)
+
 }
