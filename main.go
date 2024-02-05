2a30a2f96e4c75c498dd7d63b2911329aa4edb9c
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-05 12:07:59 +0530

    empty interface

--- main.go
+++ main.go
@@ -1,91 +1,38 @@
-package main 
+package main
 import "fmt"
 
-// interface
-type Shape interface {
-  area() float32
+// empty interface as function parameter
+func displayValue(i interface {}) {
+  fmt.Println(i)
 }
 
- // Rectangle struct implements the interface
-type Rectangle struct {
-  length, breadth float32
-}
+// function with an empty interface as its parameter
+func displayValueMultiple(i... interface {}) {
+	fmt.Println(i)
+  }
 
-// Rectangle provides implementation for area()
-func (r Rectangle) area() float32 {
-  return r.length * r.breadth
-}
-
-// Triangle struct implements the interface
-type Triangle struct {
-  base, height float32       
-}
-
-// Triangle provides implementation for area()
-func (t Triangle) area() float32 {
-    return 0.5 * t.base * t.height
-}
-
-// access method of the interface
-func calculate(s Shape) float32 {
-  return s.area()
-}
-
-// main function
 func main() {
- 
-  // assigns value to struct members
-  r := Rectangle{7, 4}
-  t := Triangle{8, 12}
- 
-  // call calculate() with struct variable rect 
-  rectangleArea := calculate(r)
-  fmt.Println("Area of Rectangle:", rectangleArea)
-  
-  triangleArea := calculate(t)
-  fmt.Println("Area of Triangle:", triangleArea)
+
+  a := "Welcome to Programiz"
+  b := 20
+  c := false
+
+  // pass string to the function 
+  displayValue(a)
+
+  // pass integer number to the function
+  displayValue(b)
+
+  // pass boolean value to the function
+  displayValue(c)
+
+  // function call with a single parameter
+  displayValueMultiple(a)
+
+  // function call with 2 parameters
+  displayValueMultiple(a, b)
+
+  // function call with 3 parameters
+  displayValueMultiple(a, b, c)
 
 }
-
-
-// What happens if the struct doesn't implement all methods of interface?
-
-// package main 
-// import "fmt"
-
-// // interface
-// type Shape interface {
-//   area() float32
-//   perimeter() float32
-// }
-
-//  // Rectangle struct implements the interface
-// type Rectangle struct {
-//   length, breadth float32
-// }
-
-// // Rectangle provides implementation for area()
-// func (r Rectangle) area() float32 {
-//   return r.length * r.breadth
-// }
-
-// // access method of the interface
-// func calculate(s Shape) float32 {
-//   return s.area()
-// }
-
-// // main function
-// func main() {
- 
-//   // assigns value to struct members
-//   r := Rectangle{7, 4}
-
-//   // call calculate() with struct variable rect 
-//   rectangleArea := calculate(r)
-//   fmt.Println("Area of Rectangle:", rectangleArea)
-// }
-
-// output : 
-
-// cannot use r (type Rectangle) as type Shape in argument to calculate:
-// 	Rectangle does not implement Shape (missing perimeter method)
