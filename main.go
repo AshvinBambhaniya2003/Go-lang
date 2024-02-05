c434634a7cad53706273a060e252f66858fab733
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-05 15:14:12 +0530

    generics

--- main.go
+++ main.go
@@ -1,53 +1,40 @@
 package main
+
 import "fmt"
 
+type Age interface {
+	int64 | int32 | float32 | float64 
+}
 
-// create struct
-type Weather struct{
-	city string
-	temperature int
-}   
+func newGenericFunc[age int64 | float64](myAge age) {
+	val := int(myAge) + 1
+	fmt.Println(val)
+}
 
-// declare a struct Person	  
-type Person struct {
-	name string
-	age int
+
+func newGenericFuncAny[age any](myAge age) {
+	// val := int(myAge) + 1  // type casting : this is also not allowed 
+	// fmt.Println(val)
+	fmt.Println(myAge)
+}
+
+func newGenericFuncInterface[age Age](myAge age) {
+	val := int(myAge) + 1
+	fmt.Println(val)
 }
 
 func main() {
+	fmt.Println("Go Generics Tutorial")
+	var testAge int64 = 23
+	var testAge2 float64 = 24.5
 
-  // instance of the struct Person
-  person1 := Person{"John", 25}
+	newGenericFunc(testAge)
+	newGenericFunc(testAge2)
+	// newGenericFunc(testString)  --> this is not allowed
 
-  // create a struct type pointer that
-  // stores the address of person1
-  var ptr *Person
-  ptr = &person1
+	var testString string = "Elliot"
 
-  // print struct instance
-  fmt.Println(person1)
-
-  // print the struct type pointer
-  fmt.Println(ptr)
-  fmt.Println(*ptr)
-  fmt.Println((*ptr).name)
-  fmt.Println(ptr.name)
-
-
-  // create struct variable
-  weather := Weather{"California", 20}
-  fmt.Println("Initial Weather:", weather)
-
-  // create struct type pointer
-  ptr1 := &weather
-
-  // change value of temperature to 25
-  ptr1.temperature = 25 
-//   (*ptr1).temperature = 25   //also work 
-
-  fmt.Println("Updated Weather:", weather)
-
-
+	newGenericFuncAny(testAge)
+	newGenericFuncAny(testAge2)
+	newGenericFuncAny(testString)
 }
-
-
