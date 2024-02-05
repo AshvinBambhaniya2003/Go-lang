45d1ae8eb35a77e7177cc1c4ce7e2f89c8d92717
AshvinBambhaniya2003 <ashvin.bambhaniya@improwised.com>
2024-02-05 12:01:56 +0530

    interface

--- main.go
+++ main.go
@@ -1,45 +1,91 @@
-package main
+package main 
+import "fmt"
 
-import (
-	"fmt"
-	// "io"
-	// "net/http"
-	"net/url"
-)
+// interface
+type Shape interface {
+  area() float32
+}
 
-const Url = "https://courses.learncodeonline.in:443/learn/account/signup?signInToPay=133333&priceId=61135"
+ // Rectangle struct implements the interface
+type Rectangle struct {
+  length, breadth float32
+}
 
+// Rectangle provides implementation for area()
+func (r Rectangle) area() float32 {
+  return r.length * r.breadth
+}
+
+// Triangle struct implements the interface
+type Triangle struct {
+  base, height float32       
+}
+
+// Triangle provides implementation for area()
+func (t Triangle) area() float32 {
+    return 0.5 * t.base * t.height
+}
+
+// access method of the interface
+func calculate(s Shape) float32 {
+  return s.area()
+}
+
+// main function
 func main() {
+ 
+  // assigns value to struct members
+  r := Rectangle{7, 4}
+  t := Triangle{8, 12}
+ 
+  // call calculate() with struct variable rect 
+  rectangleArea := calculate(r)
+  fmt.Println("Area of Rectangle:", rectangleArea)
+  
+  triangleArea := calculate(t)
+  fmt.Println("Area of Triangle:", triangleArea)
 
-	result, _ := url.Parse(Url)
-	fmt.Println(result)
-
-
-	fmt.Println(result.Scheme)
-	fmt.Println(result.Host)
-	fmt.Println(result.Path)
-	fmt.Println(result.RawQuery)
-	fmt.Println(result.Port())
-
-	qparams := result.Query()
-
-	for _, v := range qparams {
-		fmt.Println(v)
-	}
-
-	partsofUrl := &url.URL {
-		Scheme: "https",
-		Host: "ashvinbambhaniya.netlify.com",
-		Path: "/",
-		RawQuery: "",
-	}
-
-	anotherurl := partsofUrl.String()
-	fmt.Println(anotherurl)
 }
 
-func checkNilerr(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
+
+// What happens if the struct doesn't implement all methods of interface?
+
+// package main 
+// import "fmt"
+
+// // interface
+// type Shape interface {
+//   area() float32
+//   perimeter() float32
+// }
+
+//  // Rectangle struct implements the interface
+// type Rectangle struct {
+//   length, breadth float32
+// }
+
+// // Rectangle provides implementation for area()
+// func (r Rectangle) area() float32 {
+//   return r.length * r.breadth
+// }
+
+// // access method of the interface
+// func calculate(s Shape) float32 {
+//   return s.area()
+// }
+
+// // main function
+// func main() {
+ 
+//   // assigns value to struct members
+//   r := Rectangle{7, 4}
+
+//   // call calculate() with struct variable rect 
+//   rectangleArea := calculate(r)
+//   fmt.Println("Area of Rectangle:", rectangleArea)
+// }
+
+// output : 
+
+// cannot use r (type Rectangle) as type Shape in argument to calculate:
+// 	Rectangle does not implement Shape (missing perimeter method)
